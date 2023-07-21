4a0f65b7d34d0b8bd2dd8ff79a0c8cc19f800ed7
XyloxExe <maximec02@live.fr>
2023-07-21 12:26:18 +0200

    Nouvelle branche backend

--- frontend/src/utils/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateRoutes = () => {
-  const auth = localStorage.getItem("token");
-  return auth ? <Outlet /> : <Navigate to="/connexion" />;
-};
-
-export default PrivateRoutes;
